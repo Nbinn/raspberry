1ad21ba293ddb48ed07396de26d59afb81dc6d61
JackWR <jackwrion12345@gmail.com>
2023-06-03 11:13:21 +0700

    update subscriber

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ IO_USERNAME =  "jackwrion12345"
 IO_KEY = "aio_bisq78jFskfklOk7jb6e8hVAaSY4"
 
 
+
+def message(client, feed_id, payload):
+    if (feed_id == "bbc-led"):
+        print("bbc-led: ", payload)
+
+
 ## MQTT MODULE
 client = MQTTClient(IO_USERNAME , IO_KEY)
 client.connect()
 client.loop_background()
+client.on_message = message
+client.subscribe( "bbc-led" )
 
+###
 
 
 def Test():
@@ -24,10 +33,15 @@ def Test2():
     count += 1
     print ("Task2\n")
 
+
+
 def mqtt_task_1():
     value = random.randint(0, 100)
     client.publish("bbc-led", value)
-    print(value)
+    ##print(value)
+
+
+
 
 Sche = scheduler.Scheduler()
 Sche.SCH_Init()
@@ -35,6 +49,9 @@ Sche.SCH_Init()
 task1=Sche.SCH_Add_Task(Test, DELAY= 0, PERIOD=1000)
 task2=Sche.SCH_Add_Task(Test2, DELAY= 0, PERIOD=1000)
 task3 = Sche.SCH_Add_Task(mqtt_task_1, DELAY=0, PERIOD=5000)
+
+
+
 
 
 while(1):
