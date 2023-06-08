bbf14d765cde9453c8090fc0f255271cc9f1724b
JackWR <jackwrion12345@gmail.com>
2023-06-08 16:17:57 +0700

    update AI

--- physical.py
+++ physical.py
@@ -3,26 +3,33 @@ import time
 print("Sensors and Actuators")
 import serial.tools.list_ports
 
-def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "ttyUSB" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    return commPort
-    ##return "/dev/ttyUSB0"
 
-portName = getPort()
-if portName != "None":
-    try:
-        ser = serial.Serial(port=portName, baudrate=9600)
+# def getPort():
+#     ports = serial.tools.list_ports.comports()
+#     N = len(ports)
+#     commPort = "None"
+#     for i in range(0, N):
+#         port = ports[i]
+#         strPort = str(port)
+#         if "ttyUSB" in strPort:
+#             splitPort = strPort.split(" ")
+#             commPort = (splitPort[0])
+#     return commPort
+#     ##return "/dev/ttyUSB0"
 
-    except:
-        ser = serial.Serial(port="COM4", baudrate=9600)
+#
+# portName = getPort()
+
+ser = serial.Serial(port="COM4", baudrate=9600)
+ser2 = serial.Serial(port="COM8", baudrate=9600)
+
+# if portName != "None":
+
+#     try:
+#         ser = serial.Serial(port=portName, baudrate=9600)
+#         ser2 = serial.Serial(port="COM8", baudrate=9600)
+#     except:
+        
 
 
 print(ser)
@@ -40,6 +47,7 @@ def setDevice1(state):
 
 
 
+
 def serial_read_data(ser):
     bytesToRead = ser.inWaiting()
     if bytesToRead > 0:
@@ -54,26 +62,43 @@ def serial_read_data(ser):
             return -1
     return 0
 
+
+temp = 0
+mois = 0
+
 soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
 def readTemperature():
-    serial_read_data(ser)
+    global temp
+    
     ser.write(soil_temperature)
     time.sleep(1)
-    return serial_read_data(ser)
+
+    temp = serial_read_data(ser)/100
+
+    return temp
 
 soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
 def readMoisture():
-    serial_read_data(ser)
+    global mois
+    
     ser.write(soil_moisture)
     time.sleep(1)
-    return serial_read_data(ser)
+
+    mois = serial_read_data(ser)/100
+
+    return mois
 
 while True:
     print("TEST RELAY")
     print(readTemperature())
-
     ##setDevice1(True)
     time.sleep(1)
     ##setDevice1(False)
     print(readMoisture())
+
     time.sleep(1)
+
+    result = "cm4," + str(temp) + "," + str(mois)
+    ser2.write(result.encode())
+
+    time.sleep(10)
